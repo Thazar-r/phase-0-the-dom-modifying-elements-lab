0a62cccf3ad52c8af215b816215c336826a9ab85
Thazar-r <melchior.balthazar@student.moringaschool.com>
2024-06-23 23:15:43 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Melchior Balthazar is the champion";
+
+document.body.append(newHeader);
